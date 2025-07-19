0350ff7e0b52ef0135adf3122ca677369160f18f
Ojulari123 <adeoluwaojulari@gmail.com>
2025-07-19 19:41:25 -0400

    Delete user.py

--- user.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter, Depends, HTTPException, File, UploadFile
-from sqlalchemy.orm import Session
-from tables import Products,User
-from func import get_db
-from typing import List
